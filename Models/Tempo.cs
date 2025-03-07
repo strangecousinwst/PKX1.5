567cb67015101f5d34ad6397cc0db1f161b92326
strangecousinwst <139879737+strangecousinwst@users.noreply.github.com>
2025-03-07 10:44:49 +0000

    1.5 prof

---
+++ Models/Tempo.cs
@@ -0,0 +1,14 @@
+﻿namespace PKX.Models
+{
+    public class Tempo
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string? Descritivo {  get; set; }
+        public int Minutos { get; set; }
+        public int AtividadeId {  get; set; }
+        public int FuncionarioId { get; set; }
+        public int ClienteId { get; set; }
+        
+    }
+}
